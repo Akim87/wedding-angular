b5dc0b4c3452708a085a8c3ea5c7a9b6086f6c87
Akim87 <akim3496389@gmail.com>
2020-05-25 22:12:17 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { SectionHeroComponent } from './section-hero/section-hero.component';
+import { SectionServicesComponent } from './section-services/section-services.component';
+import { SectionOfferComponent } from './section-offer/section-offer.component';
+import { SectionCoachesComponent } from './section-coaches/section-coaches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
+    LogoComponent,
+    SectionHeroComponent,
+    SectionServicesComponent,
+    SectionOfferComponent,
+    SectionCoachesComponent
   ],
   imports: [
     BrowserModule,
